00a4967d76fe09ca6de426ed890c1986a0524d70
LilGibran01 <97294600+LilGibran01@users.noreply.github.com>
2022-08-11 01:59:03 +0700

    Add files via upload

---
+++ jewepe/template/dashboard.php
@@ -0,0 +1,10 @@
+<!-- Tampilan Selamat Datang -->
+<div class="container-fluid text-center mt-3">
+	<h1 class="pt-5 font-weight-bold">UNIVERSITAS JEWEPE</h1>
+	<h3 class="pb-4">Lembaga Kursus Bahasa Pemrograman</h3>
+	<img src="https://fti.gunadarma.ac.id/industri/assets/admin/images/FotoLab/Ruang%20Komputer_edit.jpg" width="60%">
+</div>
+
+</body>
+
+</html>
